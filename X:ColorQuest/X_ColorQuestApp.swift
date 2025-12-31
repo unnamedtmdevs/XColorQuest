74ef866babeabfa29cf72d5f31ae5c6867622698
ludik <bakhanetssimon@gmail.com>
2025-12-31 13:39:20 +0700

    Initial Commit

---
+++ X:ColorQuest/X_ColorQuestApp.swift
@@ -0,0 +1,17 @@
+//
+//  X_ColorQuestApp.swift
+//  X:ColorQuest
+//
+//  Created by Simon Bakhanets on 31.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct X_ColorQuestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ X:ColorQuestTests/X_ColorQuestTests.swift
@@ -0,0 +1,16 @@
+//
+//  X_ColorQuestTests.swift
+//  X:ColorQuestTests
+//
+//  Created by Simon Bakhanets on 31.12.2025.
+//
+
+import Testing
+
+struct X_ColorQuestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
